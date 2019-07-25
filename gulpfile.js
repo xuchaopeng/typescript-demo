199d6451a7289f389fa29cc9f6b19f0d34328829
xcp <m18770074875_1@163.com>
2019-07-25 14:23:47 +0800

    去掉babel的配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,40 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
+var gulp = require("gulp");
+var browserify = require("browserify");
 var source = require('vinyl-source-stream');
-var watchify = require('watchify');
-var tsify = require('tsify');
-var gutil = require('gulp-util');
+var watchify = require("watchify");
+var tsify = require("tsify");
+var gutil = require("gulp-util");
 var paths = {
-  pages: ['src/*.html']
+    pages: ['src/*.html']
 };
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
 
-var watchedBrowserify = watchify(
-  browserify({
+var watchedBrowserify = watchify(browserify({
     basedir: '.',
     debug: true,
     entries: ['src/main.ts'],
     cache: {},
     packageCache: {}
-  })
-    .plugin(tsify)
-    .transform('babelify', {
-      presets: ['env'],
-      extensions: ['.ts']
-    })
-);
+}).plugin(tsify));
 
-gulp.task('copy-html', function() {
-  return gulp.src(paths.pages).pipe(gulp.dest('dist'));
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
 });
 
 function bundle() {
-  return watchedBrowserify
-    .bundle()
-    .on('error', function(err) {
-      gutil.log(gutil.colors.red('[Error]'), err.toString());
-    })
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify())
-    .on('error', function(err) {
-      gutil.log(gutil.colors.red('[Error]'), err.toString());
-    })
-    .pipe(gulp.dest('dist'));
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps:true}))
+        .pipe(uglify())
+        // .pipe(sourcemaps.write('./')) 是否输出map文件
+        .pipe(gulp.dest("dist"));
 }
 
 gulp.task('default',gulp.series('copy-html',bundle))
-
-// gulp.task('default', ['copy-html'], bundle);
-watchedBrowserify.on('update', bundle);
-watchedBrowserify.on('log', gutil.log);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
